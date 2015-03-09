d950d3878b5fd59d43e3891b74dbc69006466c26
danielvo21 <daniel.vaquier@hotmail.com>
2015-03-09 12:15:34 +0100

    linked list

--- Programacion II/Programacion II/LinkedList.cpp
+++ Programacion II/Programacion II/LinkedList.cpp
@@ -1,5 +1,10 @@
+#ifndef LINKEDLIST_H_
+#define LINKEDLIST_H_
+#include<stdio.h>
 
-void LinkedList : add(int _value){ }
+
+void LinkedList::add(int _value){ }
 
 
-unsigned int LinkedList : count() const{ };
+unsigned int LinkedList::count() const{ };
+#endif
